8d677c3c35e84ca4f57c6c087f256e32b9ee2953
Alyson Takai <shtakai@gmail.com>
2016-05-31 15:06:04 -0700

    finished assignment

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+class Message(View):
+    def get(self, request):
+        print('Message#get')
+        context = {}
+        return render(request, 'messages/index.html', context)
+
+    def post(self, request):
+        print('Message#post')
+        print(request.POST['message_text'])
+        return HttpResponse('<h1>Message submitted {}</h1>'.format(request.POST['message_text']))
+
